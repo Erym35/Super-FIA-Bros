20c4b82f8dc36cc3c241eb6e4b26605a19e22cc8
Luca Afeltra <problast666@gmail.com>
2026-02-01 22:00:00 +0100

    Finalize project configuration and Level 1-2 support to config

--- src/run.py
+++ src/run.py
@@ -1,32 +1,52 @@
 import neat
+import os
+import warnings
+warnings.filterwarnings("ignore")
 import pickle
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import JoypadSpace
+import visualize
 import cv2
 
-def main(config_file, genome_file):
+ACTIONS = SIMPLE_MOVEMENT
+
+def main(config_file, file, level="1-1"):
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_file)
-    genome = pickle.load(open(genome_file, 'rb'))
-    env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
-    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    genome = pickle.load(open(file, 'rb'))
+    env = gym_super_mario_bros.make('SuperMarioBros-'+level+'-v0')
+    env = JoypadSpace(env, ACTIONS)
     
+    video_dir = os.path.join(os.path.dirname(__file__), 'videos')
+    os.makedirs(video_dir, exist_ok=True)
+    video_path = os.path.join(video_dir, 'mario_run.mp4')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+    height, width, _ = env.observation_space.shape
+    video = cv2.VideoWriter(video_path, fourcc, 30.0, (width, height))
+
     net = neat.nn.FeedForwardNetwork.create(genome, config)
     state = env.reset()
     done = False
     
     while not done:
-        state = cv2.resize(state, (13, 16))
-        state = cv2.cvtColor(state, cv2.COLOR_RGB2GRAY)
-        state = state.flatten()
-        output = net.activate(state)
-        action = output.index(max(output))
-        state, reward, done, info = env.step(action)
-        env.render()
+        state_input = cv2.resize(state, (13, 16))
+        state_input = cv2.cvtColor(state_input, cv2.COLOR_RGB2GRAY)
+        state_input = state_input.flatten()
         
+        output = net.activate(state_input)
+        ind = output.index(max(output))
+        state, reward, done, info = env.step(ind)
+        
+        video.write(cv2.cvtColor(state, cv2.COLOR_RGB2BGR))
+        
+    video.release()
     env.close()
 
 if __name__ == "__main__":
-    main('config', 'winner.pkl')
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--level", default="1-1")
+    args = parser.parse_args()
+    main('config', "winner.pkl", args.level)
